a89c9384bb9142bcf4516c89b4187e14dd36dea3
Wilson Huang <hjn201@gmail.com>
2016-09-16 16:05:54 +0800

    新增調整類組件的範例練習

--- CH06_03_SeekAndRatingBar/src/main/java/com/main/MainActivity.java
+++ CH06_03_SeekAndRatingBar/src/main/java/com/main/MainActivity.java
@@ -1,13 +1,38 @@
 package com.main;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Context context;
+    private SeekBar seekBar;
+    private RatingBar ratingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = this;
+
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.bt_seekBarValue:
+                Toast.makeText(context, "SeekBarValue : " + seekBar.getProgress(), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.bt_ratingBarValue:
+                Toast.makeText(context, "RatingBarValue : " + ratingBar.getProgress(), Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
