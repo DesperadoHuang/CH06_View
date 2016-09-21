52e946d08560ea597e0c1d4ee0afc9b4fc547ad7
Wilson Huang <hjn201@gmail.com>
2016-09-21 15:52:11 +0800

    新增WebView的範例練習

--- CH06_06_WebView/src/main/java/com/main/MainActivity.java
+++ CH06_06_WebView/src/main/java/com/main/MainActivity.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Context context;
@@ -25,16 +27,23 @@ public class MainActivity extends AppCompatActivity {
         webView.getSettings().setSupportZoom(true);//支援放大縮小
         webView.getSettings().setBuiltInZoomControls(true);//支援多點式觸控放大縮小
 
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                setTitle("Loading..." + newProgress + "%");
+                setProgress(newProgress * 100);
+                if (newProgress == 100) {
+                    setTitle(R.string.app_name);
+                    Toast.makeText(context, "網頁載入完成", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
-//        webView.setWebChromeClient(new WebChromeClient(){
-//            @Override
-//            public void onProgressChanged(WebView view,int Progress){
-//
-//            }
-//        });
     }
 
     public void onClick(View view) {
-
+        String url=editText.getEditableText().toString();
+        webView.loadUrl(url);
     }
+
 }
