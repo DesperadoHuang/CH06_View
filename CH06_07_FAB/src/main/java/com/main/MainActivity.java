15a8809ca115d4cc52f734d69ef06be62cfda7f3
Wilson Huang <hjn201@gmail.com>
2016-09-22 09:22:51 +0800

    新增FAB的範例練習

--- CH06_07_FAB/src/main/java/com/main/MainActivity.java
+++ CH06_07_FAB/src/main/java/com/main/MainActivity.java
@@ -1,6 +1,7 @@
 package com.main;
 
 import android.graphics.Outline;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,8 +9,11 @@ import android.view.ViewOutlineProvider;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+/**
+ *
+ */
 public class MainActivity extends AppCompatActivity {
-    private boolean play;
+    private boolean play; //紀錄FAB的狀態
     private ImageButton imageButton_FAB;
     private TextView textView;
     private String article;
@@ -24,24 +28,58 @@ public class MainActivity extends AppCompatActivity {
         textView = (TextView) findViewById(R.id.textView);
         article = getResources().getString(R.string.gueishan_island);
 
+        //實作視圖外觀
         ViewOutlineProvider viewOutlineProvider = new ViewOutlineProvider() {
             @Override
             public void getOutline(View view, Outline outline) {
+                //設定視圖尺寸
                 int size = getResources().getDimensionPixelSize(R.dimen.fab_size);
+                //設定視圖外觀
                 outline.setOval(0, 0, size, size);
             }
         };
+        //設定FAB外框樣式
         imageButton_FAB.setOutlineProvider(viewOutlineProvider);
+        //註冊FAB的onClick事件
         imageButton_FAB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //變更FAB圖樣
                 imageButton_FAB.setImageResource(play ? R.drawable.play : R.drawable.pause);
+                //改變Play/Pause狀態
                 play = !play;
+
+                //啟動文字跑馬燈
+                new Marquee().start();
             }
         });
     }
 
+    //自製的文字跑馬燈
     private class Marquee extends Thread {
+        int length = 0;
+        int maxLength = article.length();
+        private Handler handler = new Handler();
+        private Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                if (length <= maxLength && play) {
+                    textView.setText(article.subSequence(0, length));
+                    play();
+                }
+            }
+        };
 
+        @Override
+        public void run() {
+            if (play) {
+                play();
+            }
+        }
+
+        private void play() {
+            length++;
+            handler.postDelayed(runnable, 100);
+        }
     }
 }
